815389c269bef5e73ab9ad92269da2ca432580ff
Dwi Pasca <dpnursanti@gmail.com>
2021-07-06 13:01:01 +0800

    change how to selection click button

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,9 @@
 console.log("app.js is active");
-// let btnSubmit = document.getElementById("btn-submit");
-// console.log(btnSubmit);
 
-let test = () => {
-  alert("holla");
-};
+// selection element
+let btnSubmit = document.getElementById("btn-submit");
+
+// do something if the button click
+btnSubmit.addEventListener("click", () => {
+  alert("holaa");
+});
